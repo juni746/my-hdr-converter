9c9f4e883766c6302ca712507896911f905714dc
juni746 <153873708+juni746@users.noreply.github.com>
2025-12-11 22:05:17 +0400

    Add HDR video conversion function using ffmpeg
    This module provides a function to convert HDR video using ffmpeg with specified encoding and color parameters.

---
+++ ffmpeg.js
@@ -0,0 +1,21 @@
+const { exec } = require("child_process");
+
+module.exports = function convertHDR(input, output) {
+  return new Promise((resolve, reject) => {
+    const command = `
+      ffmpeg -i "${input}" \
+        -c:v libx265 -pix_fmt yuv420p10le \
+        -color_primaries bt2020 \
+        -color_trc arib-std-b67 \
+        -colorspace bt2020nc \
+        -x265-params "colorprim=bt2020:transfer=arib-std-b67:colormatrix=bt2020nc" \
+        -c:a copy \
+        "${output}"
+    `;
+
+    exec(command, (error, stdout, stderr) => {
+      if (error) return reject(error);
+      resolve(stdout);
+    });
+  });
+};
